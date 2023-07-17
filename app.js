0be965f356c23c8f8d401f46fff69054990263d0
Spawn9986 <scouch9986@gmail.com>
2023-07-17 11:55:32 -0500

    added iteration for multiple questions

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const statementContainer = document.querySelector(".statement");
 const buttons = Array.from(document.querySelectorAll("button"));
 const play = document.querySelector(".play");
 const nextBtn = document.querySelector(".next");
+//store the current results from API fetch so that I dont make an API fetch each time I call a function etc
 let currentResults;
+let currentQuestionIndex = 0;
 
 console.log(buttons);
 
@@ -16,26 +18,40 @@ async function fetchData() {
   );
 
   const resObj = await res.json();
-  const results = resObj.results[0];
+  const results = resObj.results;
 
+  console.log(results);
   return results;
 }
 
 play.addEventListener("click", async function () {
   currentResults = await fetchData();
+  //reset the current question index
+  currentQuestionIndex = 0;
 
   //grab statement out of API fetched object. Since the returned statement's HTML has entities (i.e., &quot;) we need to ensure that the HTML entities are correctly converted into their corresponding characters before rendering the text.
-  const statement = decodeHtmlEntities(currentResults.question);
+  const statement = decodeHtmlEntities(
+    currentResults[currentQuestionIndex].question
+  );
 
   //adds the statement from API fetch to statement container on screen
   statementContainer.innerText = statement;
 });
 
-//============ SUPPORTING FUNCTIONS ==========
-
 nextBtn.addEventListener("click", function () {
-  console.log("clicked nextBtn");
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < currentResults.length) {
+    const statement = decodeHtmlEntities(
+      currentResults[currentQuestionIndex].question
+    );
+
+    //adds the statement from API fetch to statement container on screen
+    statementContainer.innerText = statement;
+  }
 });
+
+//============ SUPPORTING FUNCTIONS ==========
 
 //only index 1-2 bc only wanted event listener on true & False buttons
 for (let i = 1; i < 3; i++) {
