fb5f66ece01a9d25f9cbb244f75fc09370dcb1c5
Spawn9986 <scouch9986@gmail.com>
2023-07-16 22:02:19 -0500

    got the user prefences to be feched from the API

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 const statementContainer = document.querySelector(".statement");
 const buttons = Array.from(document.querySelectorAll("button"));
-const numOfQuestions = document.querySelector("input.value");
-const difficulty = Array.from(document.querySelectorAll("select"))[1].value;
-const category = Array.from(document.querySelectorAll("select"))[0].value;
 const play = document.querySelector(".play");
 
 play.addEventListener("click", function () {
-  const difficulty = Array.from(document.querySelectorAll("select"))[1].value;
-  const category = Array.from(document.querySelectorAll("select"))[0].value;
-  console.log(category, difficulty);
+  fetchData();
 });
 
 async function fetchData() {
+  const numOfQuestions = document.querySelector("input").value;
+  const category = Array.from(document.querySelectorAll("select"))[0].value;
+  const difficulty = Array.from(document.querySelectorAll("select"))[1].value;
+  console.log(numOfQuestions);
   const res = await fetch(
-    "https://opentdb.com/api.php?amount=1&difficulty=easy&type=boolean"
+    `https://opentdb.com/api.php?amount=${numOfQuestions}&category=${category}&difficulty=${difficulty}&type=boolean`
   );
   const resObj = await res.json();
   const results = resObj.results[0];
   console.log(results);
   return results;
 }
-
-fetchData();
 
 // What I learned from this project:
 // You cant add eventlisteners to node list but you can iterate over a node list like an array and add event listeners to each one in the loop individually
