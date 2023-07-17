5c1211f5b5603002405e498be3c4a8872cef8125
Spawn9986 <scouch9986@gmail.com>
2023-07-17 09:20:30 -0500

    added an event listener for next question button

--- app.js
+++ app.js
@@ -42,6 +42,10 @@ for (let i = 1; i < 3; i++) {
   });
 }
 
+nextBtn.addEventListener("click", function () {
+  console.log("clicked nextBtn");
+});
+
 // What I learned from this project:
 // You cant add eventlisteners to node list but you can iterate over a node list like an array and add event listeners to each one in the loop individually
 // Type comparisons; trying to compare the inner text of event.target with a boolean from an object;
