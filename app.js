9f015b9846c994ae08457eebfe7200531629e340
Spawn9986 <scouch9986@gmail.com>
2023-07-16 19:36:17 -0500

    adding dom selectors

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
-const statementContainer = document.querySelector(".statement");
-const buttons = document.querySelectorAll(".options button");
-const explanationContainer = document.querySelector(".explanation");
-
 async function fetchData() {
-    const res = await fetch(
-      "https://opentdb.com/api.php?amount=1&difficulty=easy&type=boolean"
-    );
-    const resObj = await res.json();
-    console.log(resObj);
-  }
-
-function isCorrect(guess) {
-  if (guess.value === obj.answer.toString()) {
-    guess.classList.add("correct");
-  } else guess.classList.add("incorrect");
-}
-
-statementContainer.innerText = obj.statement;
-
-for (let button of buttons) {
-  button.addEventListener("click", function (e) {
-    for (let button of buttons) {
-      button.setAttribute("disabled", "");
-    }
-    explanationContainer.innerText = obj.explanation;
-
-    isCorrect(e.target);
-  });
+  const res = await fetch(
+    "https://opentdb.com/api.php?amount=1&difficulty=easy&type=boolean"
+  );
+  const resObj = await res.json();
+  const results = resObj.results[0];
+  console.log(results);
+  return results;
 }
 
 fetchData();
+
+let statementContainer = document.querySelector(".statement");
+let buttons = Array.from(document.querySelectorAll("button"));
+
 
 // What I learned from this project:
 // You cant add eventlisteners to node list but you can iterate over a node list like an array and add event listeners to each one in the loop individually
